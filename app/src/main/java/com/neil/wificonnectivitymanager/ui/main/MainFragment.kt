06f3cfbe3b09cf6c6a764148eab41babaf747166
Neil Ling <neil.ling@aylanetworks.com>
2020-09-30 18:42:29 +0800

    implements wifi manager test demo app

--- app/src/main/java/com/neil/wificonnectivitymanager/ui/main/MainFragment.kt
+++ app/src/main/java/com/neil/wificonnectivitymanager/ui/main/MainFragment.kt
@@ -1,20 +1,27 @@
 package com.neil.wificonnectivitymanager.ui.main
 
+import android.app.Dialog
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import com.neil.wificonnectivitymanager.R
+import com.wifi.connectivitymanager.WifiConnectivityManager
+import com.wifi.connectivitymanager.WifiConnectivityManagerAndroid10Imp
 
-class MainFragment : Fragment() {
+@RequiresApi(Build.VERSION_CODES.Q)
+class MainFragment : DialogFragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var wifiConnManager: WifiConnectivityManager
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -24,7 +31,11 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        wifiConnManager = WifiConnectivityManagerAndroid10Imp(context!!)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
     }
 
 }
